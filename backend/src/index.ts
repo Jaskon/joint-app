9ad8072e4ffbd7d6735a953cdad08b5254fd1b18
Eugene <jaskon97@gmail.com>
2023-08-22 01:43:34 +0200

    Push todos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,3 +14,8 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
+
+
+// TODO:
+//  1. MongoDB connection from cluster
+//  2. EC2 user data script does not start pm2 server
