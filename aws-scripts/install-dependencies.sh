0e0df704ba4522a310580cb0e53555c8e412cd16
Eugene <jaskon97@gmail.com>
2023-09-06 22:28:15 +0200

    Fix CD

--- aws-scripts/install-dependencies.sh
+++ aws-scripts/install-dependencies.sh
@@ -3,5 +3,7 @@ source /home/ec2-user/.bash_profile
 set -x
 
 
-npm install --prefix /home/ec2-user/app/frontend
-npm install --prefix /home/ec2-user/app/backend
+cd /home/ec2-user/app/frontend || exit
+npm install
+cd /home/ec2-user/app/backend || exit
+npm /home/ec2-user/app/backend
